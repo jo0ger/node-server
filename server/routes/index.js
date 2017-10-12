93369eb4ba7e8c27eddaaba029912d3481f8bea2
Jooger <zzy1198258955@163.com>
2017-10-12 19:10:31 +0800

    [update] update router

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,23 +6,33 @@
 
 'use strict'
 
-const router = require('koa-router')({
-  prefix: '/api'
-})
+const router = require('koa-router')()
 const frontend = require('./frontend')
 const backend = require('./backend')
 const { header } = require('../middleware')
+const config = require('../config')
 
 module.exports = app => {
   router.use('*', header)
 
+  router.get('/', async (ctx, next) => {
+  	ctx.body = {
+  	  name: config.name,
+  	  version: config.version,
+  	  author: config.author,
+  	  github: 'https://github.com/jo0ger',
+  	  site: config.site,
+  	  poweredBy: ['Koa2', 'MongoDB', 'Nginx']
+  	}
+  })
+
   router.use('/backend', backend.routes(), backend.allowedMethods())
   router.use(frontend.routes(), frontend.allowedMethods())
-  
+
   router.all('*', (ctx,next)=> {
     ctx.fail(404, `${ctx.path} 不支持 ${ctx.method} 请求类型`)
     ctx.status = 404
   })
-  
+
   app.use(router.routes(), router.allowedMethods())
 }
