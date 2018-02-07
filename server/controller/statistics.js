ccffc6c72c1633566fb25f686ddea93072ace2d0
jo0ger <iamjooger@gmail.com>
2018-02-07 11:48:04 +0800

    [feature] add statistics api

--- server/controller/statistics.js
+++ server/controller/statistics.js
@@ -1,8 +1,31 @@
 /**
  * @desc Statistics controller
  * @author Jooger <iamjooger@gmail.com>
- * @date 25 Sep 2017
+ * @date 2 Feb 2018
  */
 
+const { articleProxy, categoryProxy, tagProxy, userProxy, commentProxy } = require('../proxy')
+
 // TODO: 站内统计
-exports.data = async (ctx, next) => {}
+exports.data = async (ctx, next) => {
+	const data = await Promise.all([
+		articleProxy.count({ state: 1 }).exec(),
+		categoryProxy.count().exec(),
+		tagProxy.count().exec(),
+		userProxy.count().nor({ role: 0 }).exec(),
+		commentProxy.count({ type: 0 }).exec(),
+		commentProxy.count({ type: 1 }).exec()
+	]).then(([articles, categories, tags, users, comments, guestComments]) => {
+		return {
+			article: articles,
+			category: categories,
+			tag: tags,
+			user: users,
+			comment: comments,
+			guestComment: guestComments
+		}
+	})
+	data
+		? ctx.success(data, '统计信息获取成功')
+		: ctx.fail('统计信息获取失败')
+}
