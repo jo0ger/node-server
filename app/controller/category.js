1ffe1b8ec08492a0323af79f0dde513188182a69
Jooger <zzy1198258955@163.com>
2018-08-23 00:23:35 +0800

    update: category controller done

---
+++ app/controller/category.js
@@ -0,0 +1,51 @@
+/**
+ * @desc 分类 Controller
+ */
+
+const { Controller } = require('egg')
+ 
+module.exports = class CategoryController extends Controller {
+    async list () {
+        const { ctx } = this
+        const data = await this.service.category.list()
+        if (data) {
+            ctx.success(data, '分类列表获取成功')
+        } else {
+            ctx.fail('分类列表获取失败')
+        }
+    }
+
+    async item () {
+        const { ctx } = this
+        const data = await this.service.category.item()
+        if (data) {
+            ctx.success(data, '分类详情获取成功')
+        } else {
+            ctx.fail('分类详情获取失败')
+        }
+    }
+
+    async create () {
+        const { ctx } = this
+        const data = await this.service.category.create()
+        data && data.length
+			? ctx.success(data[0], '分类创建成功')
+			: ctx.fail('分类创建失败')
+    }
+
+    async update () {
+        const { ctx } = this
+        const data = await this.service.category.update()
+        data
+			? ctx.success(data, '分类更新成功')
+			: ctx.fail('分类更新失败')
+    }
+
+    async delete () {
+        const { ctx } = this
+        const data = await this.service.category.delete()
+        data
+			? ctx.success('分类删除成功')
+			: ctx.fail('分类删除失败')
+    }
+}
