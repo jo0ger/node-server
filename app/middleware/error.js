aa89c2cb59f3149957e47f46d29519f9519938e4
jo0ger <iamjooger@gmail.com>
2018-10-11 02:02:42 +0800

    fix: remove repetitive error log at global error catch function

--- app/middleware/error.js
+++ app/middleware/error.js
@@ -9,7 +9,6 @@ module.exports = (opt, app) => {
         } catch (err) {
             // 所有的异常都在 app 上触发一个 error 事件，框架会记录一条错误日志
             ctx.app.emit('error', err, ctx)
-            app.logger.error(err)
             let code = err.status || 500
             if (code === 200) code = -1
             let message = ''
